2bb04b8e9be99e253efcb9dc32d2ac11de962ea7
Akshay <sakshays.2000@gmail.com>
2024-12-27 19:30:19 +0100

    add key block

--- scripts/entities/bug.gd
+++ scripts/entities/bug.gd
@@ -19,8 +19,8 @@ var _move_tween: Tween = null
 var _current_direction := Vector2(1, 0)
 
 func _process(delta: float) -> void:
-	if _is_moving:
-		return
+	if _is_moving: return
+	
 	for direction_key in _valid_inputs.keys():
 		if Input.is_action_pressed(direction_key):
 			_current_direction = _valid_inputs[direction_key]
@@ -29,15 +29,21 @@ func _process(delta: float) -> void:
 func _move(dir: Vector2) -> void:
 	raycast.target_position = dir * _tile_size
 	raycast.force_raycast_update()
-	
-	if !raycast.is_colliding():
+
+	if abs(_current_direction.dot(Vector2(cos(sprite.rotation), sin(sprite.rotation))) - 1) > 0.01:
 		if _move_tween: _move_tween.kill()
 		_move_tween = create_tween()
 		var angle = lerp_angle(sprite.rotation, atan2(dir.y, dir.x), 1) 
 		_move_tween.tween_property(sprite, "rotation", angle, _rotation_animation_sec)
-		
-		_move_tween.tween_property(self, "position",
-			position + dir * _tile_size, _move_animation_sec).set_trans(Tween.TRANS_SINE)
-		_is_moving = true
-		await _move_tween.finished
-		_is_moving = false
+	else:
+		if !raycast.is_colliding():
+			_move_tween = create_tween()
+			_move_tween.tween_property(self, "position",
+				position + dir * _tile_size, _move_animation_sec).set_trans(Tween.TRANS_SINE)
+			_is_moving = true
+			await _move_tween.finished
+			_is_moving = false
+		else:
+			var object := raycast.get_collider() as GridObject
+			if object:
+				object.push(source, dir)
---
+++ scripts/entities/grid_object.gd
@@ -0,0 +1,5 @@
+extends CharacterBody2D
+class_name GridObject
+
+func push(source: Node2D, direction: Vector2) -> void:
+	return
