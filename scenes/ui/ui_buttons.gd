b7cbc05178f6a41300cc7f2205787a665da4f98e
Akshay <sakshays.2000@gmail.com>
2025-01-02 21:18:05 +0100

    add help screen

--- scenes/ui/ui_buttons.gd
+++ scenes/ui/ui_buttons.gd
@@ -1,5 +1,13 @@
 extends Control
 
+var tween: Tween = null
+@onready var help = $HelpScreen
+
+func _ready() -> void:
+	$HelpScreen/CloseButton.pressed.connect(_on_button_pressed)
+	help.hide()
+	help.modulate.a = 0
+	help.z_index = 2
 
 func _on_home_pressed() -> void:
 	LevelManager.load_level(0)
@@ -8,4 +16,15 @@ func _on_retry_pressed() -> void:
 	LevelManager.reload_level()
 
 func _on_help_pressed() -> void:
-	pass # Replace with function body.
+	help.show()
+	if tween: tween.kill()
+	tween = create_tween()
+	tween.tween_property(help, "modulate:a", 1., 0.5)
+	await tween.finished
+
+func _on_button_pressed() -> void:
+	if tween: tween.kill()
+	tween = create_tween()
+	tween.tween_property(help, "modulate:a", 0., 0.5)
+	await tween.finished
+	help.hide()
